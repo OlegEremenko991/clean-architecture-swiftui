48e5212bcf82a25777027b615286f7ff95d049cd
Oleg991 <o.n.eremenko@gmail.com>
2022-04-16 11:52:46 +0300

    Fixed top inset broken with .blur

--- CountriesSwiftUI/UI/RootViewModifier.swift
+++ CountriesSwiftUI/UI/RootViewModifier.swift
@@ -12,15 +12,11 @@ import Combine
 // MARK: - RootViewAppearance
 
 struct RootViewAppearance: ViewModifier {
-    
     @Environment(\.injected) private var injected: DIContainer
-    @State private var isActive: Bool = false
     internal let inspection = Inspection<Self>()
     
     func body(content: Content) -> some View {
         content
-            .blur(radius: isActive ? 0 : 10)
-            .onReceive(stateUpdate) { self.isActive = $0 }
             .onReceive(inspection.notice) { self.inspection.visit(self, $0) }
     }
     
